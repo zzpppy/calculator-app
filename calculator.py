0ab83f12041b38afbb457cce4d6a386565106bdb
zzpppy <zzpppy@gmail.com>
2026-05-26 15:38:15 +0800

    feat: 实现乘法

--- calculator.py
+++ calculator.py
@@ -1,7 +1,14 @@
-ef add(a, b):
+def add(a, b):
     return a + b
 def subtract(a, b):
     return a - b
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        return "Error: division by zero"
+    return a / b
 if __name__ == "__main__":
     print(add(10, 5))
     print(subtract(10, 5))
